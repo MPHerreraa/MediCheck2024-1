0290130802a23b1054fd9f56bfb266ef17652afa
MP_Herrera <153849807+MPHerreraa@users.noreply.github.com>
2024-05-28 10:26:01 -0300

    Conexión con la base de datos en firebase y api get de eventos (sprint 1 parece terminado)

--- src/app.js
+++ src/app.js
@@ -1,8 +1,27 @@
+// Importación de express (framework web para Node.js) y morgan (middleware para registrar peticiones HTTP)
 const express = require('express'); 
 const morgan = require('morgan'); 
 
+// Importación de la constante db del archivo firebase.js 
+const {db} = require('./firebase') 
+
+// Declaración de la constante app, la cual permite utilizar express 
 const app = express(); 
+
 
 app.use(morgan('dev')) 
 
-module.exports = app; 
+// En cuanto carga la página, se realiza una petición GET 
+app.get('/', async (req, res) => {
+    // Se guardan los valores de la colección Eventos en la constante querySnapshot (lo primero que se verá en la página)
+    const querySnapshot = await db.collection('Eventos').get()
+
+    // Se imprimen por consola el nombre y el valor de los elementos de querySnapshot 
+    console.log(querySnapshot.docs[0].data()); 
+
+    // Envía una respuesta a la petición diciendo "Hola"
+    res.send('Hola')
+})
+
+// Exporta la constante app para que ezpress pueda ser utilizado en otros archivos
+module.exports = app;
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,21 @@
-require('dotenv').config 
+// Importación del módulo dotenv para cargar variables del archivo .env
+require('dotenv').config(); 
 
-const {initializeApp, applicationDefault} = require('firebase-admin/app') 
+// Importación de las funciones necesarias de firebase-admin para inicializar la aplicación firebase
+const {initializeApp, applicationDefault} = require('firebase-admin/app'); 
+
+// Importación de la función para instanciar Firestore
+const {getFirestore} = require('firebase-admin/firestore'); 
+
+// Inicialización de Firebase con sus credenciales
 initializeApp({
-    credential: applicationDefault
+    credential: applicationDefault(), 
 })
+
+// Declaración de la constante db (database) 
+const db = getFirestore() 
+
+// Exportación de la constante db para que pueda ser utilizada en otros archivos 
+module.exports = {
+    db, 
+}
